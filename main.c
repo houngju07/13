3c64aaf36b0eeda3428d9dfb64ab7c1ed64859b9
- <->
2020-11-30 23:37:53 +0900

    3

--- main.c
+++ main.c
@@ -1,8 +1,47 @@
+#define MAX_NAME 30
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct keytab{
+	int nums;
+	char name[MAX_NAME];
+};
+
+struct keytab keywords[13]={
+	{0,"int"},
+	{0,"float"},
+	{0,"return"},
+	{0,"if"},
+	{0,"for"},
+	{0,"while"},
+	{0,"goto"},
+	{0,"switch"},
+	{0,"void"},
+	{0,"default"},
+	{0,"char"},	
+	{0,"do"}		
+};
+
+int count_word(char* word){
+	int i;
+	for(i=0;i<13;i++){
+			if(strncmp(keywords[i].name, strlen(keywords[i].name))==0){
+				keywords[i].nums++;
+				break;
+			}		
+	}
+	return 0;	
+}
+
+void print_word(void){
+	int i;
+	for(i=0;i<13;i++){
+		printf("%s : %i\n", keywords[i].name, keywords[i].nums);	
+	}	
+}
+
 int is_whitespace(char c){
 	if (c ==' '||
 		c== '\n'||
